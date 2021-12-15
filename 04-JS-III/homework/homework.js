6e08380a53f1f1adb2b0b0d32d2bf745e85c0bd5
William Mauricio Suarez <williamsuarez@tronex.com>
2021-12-15 16:04:17 -0500

    revisión JSIII

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,8 +3,10 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  let fruits = ['manzana', 'naranja'] {
+    let first = fruits [0]
+  }
 }
-
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
